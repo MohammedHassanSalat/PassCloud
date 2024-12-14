012fd0a2b16a2f160a3c2add8a16b314f0ba9b49
MohammedHassanSalat <mohh20033@gmail.com>
2024-12-14 02:20:54 +0200

    setup environment & components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"password-manager-2b55d","appId":"1:818985690594:web:3d984dee2646015e4d05bb","storageBucket":"password-manager-2b55d.firebasestorage.app","apiKey":"AIzaSyBAJuY5SrXhjwDyjIm0sG2M8bjCiuiWJH4","authDomain":"password-manager-2b55d.firebaseapp.com","messagingSenderId":"818985690594"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging())]
 };
